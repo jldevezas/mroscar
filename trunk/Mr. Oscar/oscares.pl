a3486d1fcc75f990038e52f9845d887eea73f061
joseluisdevezas <joseluisdevezas@1ae5d266-0fbe-11de-a3bf-55d8bd1d7a4a>
2009-03-16 13:31:16 +0000

    Alteração de identificadores para human-readable, para efeitos de output e evitar duplicação de dados.

--- trunk/Mr. Oscar/oscares.pl
+++ trunk/Mr. Oscar/oscares.pl
@@ -128,28 +128,45 @@ verbo(s,Sujeito,ganhar) --> [ganhou],{verifica_sujeito(Sujeito);assert(erro(sema
 verbo(s,_,ser) --> [foi].
 
 % Filmes
-nome(m-s,thedarkknight) --> ['The','Dark','Knight'].
-nome(m-s,nocountryforoldmen) --> ['No','Country',for,'Old','Men'].
+nome(m-s,'The Dark Knight') --> ['The','Dark','Knight'].
+nome(m-s,'No Country for Old Men') --> ['No','Country',for,'Old','Men'].
+nome(f-s,'The Golden Compass') --> ['The','Gold','Compass'].
+nome(m-s,'There Will Be Blood') --> ['There','Will','Be','Blood'].
+nome(m-s,'Michael Clayton') --> ['Michael','Clayton'].
+nome(m-s,'Juno') --> ['Juno'].
 
 % Actores
-nome(m-s, danieldaylewis) --> ['Daniel', 'Day-Lewis'].
+nome(m-s, 'Daniel Day-Lewis') --> ['Daniel', 'Day-Lewis'].
 
 % Realizadores
-nome(p-s, irmaoscoen) --> [irmãos, 'Coen'];['Irmãos', 'Coen'].
+nome(p-s, 'Irmãos Coen') --> [irmãos, 'Coen'];['Irmãos', 'Coen'].
 
 % Prémios
-nome(m-s,melhorfilme) --> [melhor,filme].
-nome(m-s,melhorrealizador) --> [melhor,realizador].
-nome(m-s,melhoractor) --> [melhor,actor].
-nome(f-s,melhoractriz) --> [melhor,actriz].
-nome(m-s,melhoractorsec) --> [melhor,actor,secundário].
-nome(f-s,melhoractrizsec) --> [melhor,actriz,secundária].
-nome(m-s,melhorargorig) --> [melhor,argumento,original].
-nome(m-s,melhorargadapt) --> [melhor,argumento,adaptado].
-nome(m-s,melhorfilmeanim) --> [melhor,filme,de,animação].
-nome(m-s,melhorfilmelingestr) --> [melhor,filme,em,lingua,estrangeira].
-nome(f-s,melhorfotografia) --> [melhor,fotografia].
-nome(f-s,melhormisturasom) --> [melhor,mistura,de,som].
+nome(m-s,'Melhor Filme') --> [melhor,filme].
+nome(m-s,'Melhor Realizador') --> [melhor,realizador].
+nome(m-s,'Melhor Actor') --> [melhor,actor].
+nome(f-s,'Melhor Actriz') --> [melhor,actriz].
+nome(m-s,'Melhor Actor Secundário') --> [melhor,actor,secundário].
+nome(f-s,'Melhor Actriz Secundária') --> [melhor,actriz,secundária].
+nome(m-s,'Melhor Argumento Original') --> [melhor,argumento,original].
+nome(m-s,'Melhor Argumento Adaptado') --> [melhor,argumento,adaptado].
+nome(m-s,'Melhor Filme de Animação') --> [melhor,filme,de,animação].
+nome(m-s,'Melhor Filme em Lingua Estrangeira') --> [melhor,filme,em,lingua,estrangeira].
+nome(f-s,'Melhor Fotografia') --> [melhor,fotografia].
+nome(f-s,'Melhor Direcção Artística') --> [melhor,direcção,artística].
+nome(m-s,'Melhor Guarda-Roupa') --> [melhor,guarda-roupa].
+nome(f-s,'Melhor Edição') --> [melhor,edição].
+nome(f-s,'Melhor Caracterização') --> [melhor,caracterização].
+nome(m-p,'Melhores Efeitos Visuais') --> [melhores,efeitos,visuais].
+nome(f-s,'Melhor Montagem de Som') --> [melhor,montagem,de,som].
+nome(f-s,'Melhor Mistura de Som') --> [melhor,mistura,de,som].
+nome(f-s,'Melhor Banda Sonora') --> [melhor,banda,sonora].
+nome(f-s,'Melhor Canção Original') --> [melhor,canção,original].
+nome(m-s,'Melhor Documentário') --> [melhor,documentário].
+nome(m-s,'Melhor Documentário em Curta-Metragem') --> [melhor,documentário,em,curta-metragem].
+nome(f-s,'Melhor Curta Metragem') --> [melhor,curta-metragem].
+nome(f-s,'Melhor Curta Metragem de Animação') --> [melhor,curta-metragem,de,animação].
+nome(m-s,'Óscar Honorário) --> [óscar,honorário].
 
 % Vocabulário geral
 nome(m-s,premio) --> [prémio].
@@ -159,26 +176,31 @@ nome(m-s,nomeado) --> [nomeado].
 % BASE DE CONHECIMENTO
 
 % Filmes
-filme(thedarkknight).
-filme(juno).
-filme(michaelclayton).
-filme(therewillbeblood).
-filme(nocountryforoldmen).
+filme('The Dark Knight').
+filme('Juno').
+filme('Michael Clayton').
+filme('There Will Be Blood').
+filme('No Country for Old Men').
+filme('The Golden Compass').
 
 % Actores
-actor(danieldaylewis).
+actor('Daniel Day-Lewis').
 
 % Realizadores
-realizador(irmaoscoen).
+realizador('Irmãos Coen').
 
 % Prémios
-ganhar(nocountryforoldmen,melhorfilme,'No Country for Old Men').
-ganhar(danieldaylewis,melhorrealizador,'Daniel Day-Lewis').
-ganhar(thedarkknight,melhormisturasom,'The Dark Knight').
+ganhar(nocountryforoldmen,'Melhor Filme','No Country for Old Men').
+ganhar(danieldaylewis,'Melhor Realizador','Daniel Day-Lewis').
+ganhar(thedarkknight,'Melhor Mistura de Som','The Dark Knight').
+ganhar(thegoldencompass,'Melhores Efeitos Visuais','The Golden Compass').
 
 % Nomeados
-nomeado(atonement,melhorfilme,'Atonement').
-nomeado(juno,melhorfilme,'Juno').
-nomeado(michaelclayton,melhorfilme,'Michael Clayton').
-nomeado(therewillbeblood,melhorfilme,'There Will Be Blood').
-nomeado(nocountryforoldmen,melhorfilme,'No Country for Old Men').
+
+nomeado(atonement,'Melhor Filme','Atonement').
+nomeado(juno,'Melhor Filme','Juno').
+nomeado(michaelclayton,'Melhor Filme','Michael Clayton').
+nomeado(therewillbeblood,'Melhor Filme','There Will Be Blood').
+nomeado(nocountryforoldmen,'Melhor Filme','No Country for Old Men').
+
+nomeado(thegoldencompass,'Melhores Efeitos Visuais','The Golden Compass').
